ab65a439a032111ee6603e5aff0714ac2eb1068f
Anshul9602 <anshulkumar242507@gmail.com>
2024-01-17 13:02:26 +0530

    add new

--- application/views/main.php
+++ application/views/main.php
@@ -21,10 +21,10 @@
       <!-- Centered Text Overlay -->
       <div
         style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; color: white; z-index: 2;">
-        <h1 style="margin: 0; font-size: 6em; --fontSize: 26; ">REAL ESTATE</h1>
+        <h1 style="margin: 0; font-size: 6em; --fontSize: 26; ">FINANCE</h1>
         <h3 class="fusion-title-heading title-heading-center fusion-responsive-typography-calculated"
           style="margin: 0; font-size: 1.5em; --fontSize: 26; line-height: 1.2;">
-          <span style="letter-spacing: 2px; line-height: 36px;"> FINANCE <strong>|</strong> INVESTMENT
+          <span style="letter-spacing: 2px; line-height: 36px;"> FINANCE <strong>|</strong> ACCOUNTING
             <strong>|</strong> DEVELOPMENT<span></span></span>
         </h3>
       </div>
@@ -41,16 +41,121 @@
         <h3 style="color: rgb(255, 255, 255);font-Size: 22; line-height: 1.91; " class="mt-4"><em>What defines us is not
             what we do, but how we do it.</em></h3>
 
-        <p class="mt-5" style="color:#fff;">We embrace the unknown as an opportunity to create something meaningful and
-          enriching. Whether developing a thriving community in an underserved area, partnering with a creative
-          architectural pioneer or building value where there was none, the unknown is always an opportunity for us.</p>
+        <p class="mt-5" style="color:#fff;">
+          ADLedger is dedicated to fostering growth and innovation at every level. Our mission is to create a
+          collaborative work environment that promotes personal and professional development for both our clients and
+          team members.</p>
       </div>
 
     </div>
 
   </div>
-</section>
 
+  <div class="container">
+
+    <style>
+      .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        min-height: 270px;
+      }
+
+      .count {
+        background-image: url('assets/img/dm1.png');
+        padding: 40px 12px;
+        min-height: 200px;
+        width: 100%;
+        margin: auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-content: center;
+        text-align: center;
+      }
+    </style>
+
+
+    <div class="row mt-md-5 mt-3 pb-md-5 pb-3">
+      <div class="col-md-12">
+        <h2 class="text-center" style="width: 100%;">Why Choose ADLedger Outsourcing?</h2>
+        <p class="text-center mt-3" style="width: 100%;">ADLedger provides trusted Outsourcing Services for bookkeeping,
+          payroll, and tax preparation.</p>
+      </div>
+      <div class="col-md-4 p-5">
+        <div class="card p-md-4 p-3">
+          <img src="" alt="">
+          <h6 class="pt-4 bold" style="    font-weight: 600;">DAY-TO-DAY ACCOUNTING ENTRIES IN SOFTWARES</h6>
+          <p class="pt-3">Efficient and accurate data entry services using Tally, QB & many more ERP software ensuring
+            precise and up-to-date financial records.</p>
+        </div>
+      </div>
+      <div class="col-md-4 p-5">
+        <div class="card p-md-4 p-3">
+          <img src="" alt="">
+          <h6 class="pt-4 bold" style="    font-weight: 600;">QUICKBOOKS SUPPPORT</h6>
+          <p class="pt-3">Specialized assistance in QuickBooks for streamlined financial management and reporting.</p>
+        </div>
+      </div>
+      <div class="col-md-4 p-5">
+        <div class="card p-md-4 p-3">
+          <img src="" alt="">
+          <h6 class="pt-4 bold" style="    font-weight: 600;">EXCEL ACCOUNTING & REPORTING</h6>
+          <p class="pt-3">Tailored Excel solutions for comprehensive and customizable accounting needs.</p>
+        </div>
+      </div>
+    </div>
+
+
+
+  </div>
+
+</section>
+<section style="background-color:#46387b;">
+  <div class="container">
+    <div class="row text-white">
+      <div class="col-md-5">
+        <h6>+12 Years Of Experience</h6>
+        <h3 class="mt-4" style="font-weight: 600;">
+          ADLedger offers comprehensive services in bookkeeping, payroll, and tax preparation</h3>
+        <p class="mt-4">ADLedger is a finance and accounting-focused company, specializing in providing comprehensive services for CPA firms and businesses of various sizes. Our expertise lies in delivering top-notch financial and accounting solutions, supported by the latest technologies. We understand the significance of even routine financial and accounting tasks, and our commitment is to enhance the overall business performance of our clients through our dedicated services.</p>
+      </div>
+
+      <div class="col-md-7 row" style="    align-items: center;">
+        <div class="col-md-4 col-4">
+          <div class="count">
+            <h2>1+</h2>
+            <h6 class="mt-2">Years Of Work Experience</h6>
+          </div>
+        </div>
+        <div class="col-md-4 col-4">
+          <div class="count">
+            <h2>100+</h2>
+            <h6 class="mt-2">Hours Of Bookkeeping, Payroll & Tax Return Processing</h6>
+          </div>
+        </div>
+        <div class="col-md-4 col-4">
+          <div class="count">
+            <h2>30+</h2>
+            <h6 class="mt-2">Client Projects</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+<section class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <img src="assets/img/blog1.png" alt="" width="100%">
+    
+      <h6 class="mt-3" style="font-weight: 600;">Director &nbsp;-&nbsp;<span style="font-weight:200;font-size:small;">13-01-2024</span></h6>
+      <h3>QUICKBOOKS SUPPPORT</h3>
+      <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati enim necessitatibus aut commodi sequi ....
+      </p>
+    
+    </div>
+  </div>
+</section>
 
 
 
