b4dada72f82156050c4647e12a5b6b42a71754e5
Anshul9602 <anshulkumar242507@gmail.com>
2024-02-15 17:22:02 +0530

    form submit  main

---
+++ application/views/service.php
@@ -0,0 +1,377 @@
+
+<style>
+  .mainb {
+    background-image: url(assets/img/ab.jpg);
+    background-position: 50% 235%;
+    background-size: cover;
+    height: 50vh;
+  }
+
+  .section-title-w {
+    padding-bottom: 40px;
+    margin-top: 30vh;
+  }
+
+  .section-title-w h2 {
+    color: #f1f1f1;
+    font-size: 14px;
+    font-weight: 500;
+    padding: 0;
+    margin: 0 0 5px 0;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+  }
+
+  .section-title-w p {
+    color: #fff;
+    font-size: 36px;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-family: "Poppins", sans-serif;
+  }
+</style>
+<style>
+    .ag-format-container {
+      width: 1142px;
+      margin: 0 auto;
+    }
+    
+
+    .ag-courses_box {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-align: start;
+      -ms-flex-align: start;
+      align-items: flex-start;
+      -ms-flex-wrap: wrap;
+      flex-wrap: wrap;
+    
+      padding: 50px 0;
+    }
+    .ag-courses_item {
+      -ms-flex-preferred-size: calc(33.33333% - 30px);
+      flex-basis: calc(33.33333% - 30px);
+    
+      margin: 0 15px 30px;
+    
+      overflow: hidden;
+    
+      border-radius: 28px;
+    }
+    .ag-courses-item_link {
+      display: block;
+      padding: 30px 20px;
+      background-color: #00243a;
+    
+      overflow: hidden;
+    
+      position: relative;
+    }
+    .ag-courses-item_link:hover,
+    .ag-courses-item_link:hover .ag-courses-item_date {
+      text-decoration: none;
+      color: #FFF;
+    }
+    .ag-courses-item_link:hover .ag-courses-item_bg {
+      -webkit-transform: scale(10);
+      -ms-transform: scale(10);
+      transform: scale(10);
+    }
+    .ag-courses-item_title {
+      min-height: 100px;
+    
+    
+      overflow: hidden;
+    
+ 
+      font-size: 18px;
+      color: #FFF;
+    
+      z-index: 2;
+      position: relative;
+    }
+    .ag-courses-item_date-box {
+      font-size: 18px;
+      color: #FFF;
+    
+      z-index: 2;
+      position: relative;
+    }
+    .ag-courses-item_date {
+      font-weight: bold;
+      color: #f9b234;
+    
+      -webkit-transition: color .5s ease;
+      -o-transition: color .5s ease;
+      transition: color .5s ease
+    }
+    .ag-courses-item_bg {
+      height: 128px;
+      width: 128px;
+      background-color: #f9b234;
+    
+      z-index: 1;
+      position: absolute;
+      top: -75px;
+      right: -75px;
+    
+      border-radius: 50%;
+    
+      -webkit-transition: all .5s ease;
+      -o-transition: all .5s ease;
+      transition: all .5s ease;
+    }
+    .ag-courses_item:nth-child(2n) .ag-courses-item_bg {
+      background-color: #3ecd5e;
+    }
+    .ag-courses_item:nth-child(3n) .ag-courses-item_bg {
+      background-color: #e44002;
+    }
+    .ag-courses_item:nth-child(4n) .ag-courses-item_bg {
+      background-color: #952aff;
+    }
+    .ag-courses_item:nth-child(5n) .ag-courses-item_bg {
+      background-color: #cd3e94;
+    }
+    .ag-courses_item:nth-child(6n) .ag-courses-item_bg {
+      background-color: #4c49ea;
+    }
+    
+    
+    
+    @media only screen and (max-width: 979px) {
+      .ag-courses_item {
+        -ms-flex-preferred-size: calc(50% - 30px);
+        flex-basis: calc(50% - 30px);
+      }
+      .ag-courses-item_title {
+        font-size: 24px;
+      }
+    }
+    
+    @media only screen and (max-width: 767px) {
+      .ag-format-container {
+        width: 96%;
+      }
+    
+    }
+    @media only screen and (max-width: 639px) {
+      .ag-courses_item {
+        -ms-flex-preferred-size: 100%;
+        flex-basis: 100%;
+      }
+      .ag-courses-item_title {
+        min-height: 72px;
+        line-height: 1;
+    
+        font-size: 24px;
+      }
+      .ag-courses-item_link {
+        padding: 22px 40px;
+      }
+      .ag-courses-item_date-box {
+        font-size: 16px;
+      }
+    }
+  </style>
+
+  <div class="mainimg" style="position: relative;background:black;">
+    <img src="assets/img/fin.jpg" alt="" width="100%" style="    opacity: 0.4;">
+    <div class="over" style="position: absolute;  top: 50%;left: 50%; transform: translate(-50%, -50%)">
+      <h1 class="h0 mt-30 mt-4"> <span class="orange text-white">Finance</span></h1>
+    </div>
+  </div>
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="text-center pb-3" style="font-weight: 700;">Bookkeeping</h1>
+        <p class="text-center">Remote Bookkeeping, recording financial transactions, including purchases, sales, receipts, and payments.</p>
+      </div>
+      <div class="col-md-12">
+        <div class="ag-format-container">
+          <div class="ag-courses_box">
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                1)<br>	Recording of all transactions related to expenses, income, assets & liabilities
+                </div>
+      
+                
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                2)<br>	Preparation of general ledger and subsidiary ledgers
+                </div>
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                3)<br>	Assets accounting and maintenance of fixed assets register
+                </div>
+      
+               
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                4)<br>	Accounts receivables and age-wise reporting to management
+                </div>
+      
+                
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                5)<br>	Accounts payables including overdue liabilities.
+                </div>
+      
+                
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                6)<br>	Bank reconciliation
+                </div>
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg">
+                </div>
+                <div class="ag-courses-item_title">
+                7)<br>	Trial balance
+                </div>
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                8)<br>	Income statement (Profit & Loss)
+                </div>
+      
+                
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+               9)<br>	Balance-sheet
+                </div>
+      
+                
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                10)<br>	Cash flow statement
+                </div>
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                11)<br>	Cost Report and Analysis
+                </div>
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                12)<br>	Preparation of Annual Financial statements
+                </div>
+      
+                
+              </a>
+            </div>
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                13)<br>	MIS – including Ageing report & summaries
+                </div>
+      
+                
+              </a>
+            </div>
+      
+          
+      
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+      
+                <div class="ag-courses-item_title">
+                14)<br>	Preparation of various reports required for compliance under applicable Rules & Regulations such as GST/TDS etc.
+                </div>
+      
+               
+              </a>
+            </div>
+      
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <img src="assets/img/fin1.jpg" alt="" width="100%">
+        </div>
+        <div class="col-md-6">
+          <p class="">
+            <span class="h3" style="font-weight: 800;">2.	Financial Statements - </span>
+            Preparation of financial statements such as balance sheets, income statements, and cash flow statements to provide a snapshot of a company's financial health.
+          </p>
+          <p class="pt-3">
+            <span class="h3" style="font-weight: 800;">3.	Tax Services - </span>
+            Ensuring compliance with tax regulations, and advising on tax planning strategies to optimize financial outcomes.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  <!-- partial:index.partial.html -->
+
