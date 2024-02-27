4beab25632cd37b0fb523decb9ac985732074c28
Anshul9602 <anshulkumar242507@gmail.com>
2024-02-27 11:17:32 +0530

    add new artical

--- application/views/news.php
+++ application/views/news.php
@@ -129,5 +129,108 @@
 
 
         </div>
+        <br><br>
+        <div class="container border">
+            <div class="entry-content p-4">
+
+                <h1 style="color: #09445a; margin-bottom: 10px;    font-weight: 700;">
+                    Upholding Integrity: The Cornerstone of Accounting Ethics<br>
+
+                </h1>
+                <p>Short Analysis
+                </p>
+                <div style="height: 4px;background-color:#9cc4e4;margin:0 5px;"></div>
+                <div class="item-media post-thumbnail mt-3">
+                    <img src="assets/img/news1.jpg" alt="img">
+                </div> <br>
+
+                <div class="row">
+                    <div class="col-md-8 mt-md-4 mt-3">
+                        <div class="post-content">
+                            <h4 class="pb-md-4 pb-2" style="color: #09445a; margin-bottom: 10px;    font-weight: 700;">
+                                Upholding Integrity: The Cornerstone of Accounting Ethics</h4>
+                            <div>
+                                <h5 class="pb-2" style="font-weight: 600;">The Importance of Ethical Accounting
+                                    Practices</h5>
+                                <p>Accounting is more than just number-crunching; it's about providing accurate,
+                                    reliable information that enables informed decision-making. Ethical accounting
+                                    practices ensure that financial data is not only accurate but also presented in a
+                                    fair and transparent manner. This is essential for fostering trust and confidence
+                                    among investors, creditors, and other stakeholders.</p>
+                            </div>
+                            <div class="mt-5">
+                                <h5 class="pb-2" style="font-weight: 600;">Key Principles of Accounting Ethics</h5>
+                                <p><span style="font-weight: 700;">1. Integrity:</span> Integrity forms the backbone of
+                                    ethical accounting. It all professional and business relationships, ensuring that
+                                    financial information is not manipulated or represented in such fashion not to gain
+                                    personal interest.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-md-4 mt-3"
+                        style="    background: #9cc4e4;">
+                        <br>
+                        <div style="padding: 20px;">
+                            <h6 style="    font-size: larger;">At ADLedger Services Pvt. Ltd., we understand that ethics isn't just a buzzword; it's
+                                the very foundation upon which the accounting profession stands. In an era where
+                                transparency and accountability are paramount, maintaining the highest standards of
+                                ethics is not just a choice, but a responsibility we owe to our clients, stakeholders,
+                                and society at large.</h6>
+
+                        </div>
+                    </div>
+                    <div class="col-md-12 row">
+                        <div class="col-md-6 mt-3">
+                            <div class="mt-2">
+                               
+                                <p><span style="font-weight: 700;">2. Objectivity:</span> Objectivity requires accountants to remain impartial and unbiased, which should not compromise their judgment. This means making decisions based solely on factual facts and judgment, rather than personal biases or outside influences.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-3">
+                            <div class="mt-2">
+                               
+                                <p><span style="font-weight: 700;">3. Confidentiality:</span>  Confidentiality is crucial in maintaining trust between accountants and their clients. Accountants must respect the confidentiality of client’s information and refrain from disclosing sensitive data.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-3">
+                            <div class="mt-2">
+                               
+                                <p><span style="font-weight: 700;">4. Professional Competence and Due Care:</span> Professional competence involves staying up to date with the latest accounting standards, regulations, and industry trends. Accountants will exercise due care by diligently performing their duties and applying their expertise to the best of their abilities.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-3">
+                            <div class="mt-2">
+                               
+                                <p><span style="font-weight: 700;">5. Professional Behaviour:</span> Professional behaviour encompasses acting in a manner that upholds the reputation of the accounting profession. This includes treating colleagues, clients, and other stakeholders with respect and courtesy, as well as refraining from engaging in activities which could bring discredit to the profession.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-5">
+                        <h4 class="pb-2 text-center" style="font-weight: 600;" >Our Commitment to Ethical Excellence</h4>
+                        <p class="text-center">At ADLedger Services Pvt. Ltd., we are committed to upholding the highest standards of ethics in all aspects of our work. Our team of experienced professionals adheres strictly to the principles of integrity, objectivity, confidentiality, professional competence, and professional behaviour.</p>
+                        <p class="mt-3 text-center">
+                            We recognize that lapses can have serious consequences, not only for our clients but also for our reputation and the accounting profession. That's why we have implemented rigorous internal controls and guidelines to ensure compliance with regulatory requirements and industry best practices.
+                        </p>
+                    </div>
+                    <div class="col-md-12 mt-5">
+                        <h4 class="pb-2 text-center" style="font-weight: 600;" >Conclusion</h4>
+                        <p class="text-center">Ethical accounting practices are not just a moral imperative; they are essential for the sustainability and credibility of the accounting profession. At ADLedger Services Pvt. Ltd., we pride ourselves on our unwavering commitment to ethics and integrity, and we are dedicated to serving our clients with honesty, transparency, and professionalism always. When you choose us as your trusted partner, you can rest assured that your financial interests are in safe hands.</p>
+                        
+                    </div>
+                </div>
+
+                <div class="col-md-12 pt-5">
+                    <div class="post-content">
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
     </div>
 </section>
